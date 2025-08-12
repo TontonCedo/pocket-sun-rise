c5f3f77335673ed938037863da677d7d1795ef64
CedricLanglois <cedriclanglois@icloud.com>
2025-08-12 16:15:04 +0200

    Initial commit

---
+++ src/api/sub-region/controllers/sub-region.ts
@@ -0,0 +1,7 @@
+/**
+ * sub-region controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::sub-region.sub-region');
---
+++ src/api/sub-region/routes/sub-region.ts
@@ -0,0 +1,7 @@
+/**
+ * sub-region router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::sub-region.sub-region');
